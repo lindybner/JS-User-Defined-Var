8b7d3a7bdfdf31fec0862cf372c4266d4f1f0b60
Jim Lin-Dybner <lin.jycl@gmail.com>
2024-03-08 16:06:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-window.onload = () => {};
+window.onload = () => {
+  let userInput;
+  let msgPrompt = `Please enter a variable name.`;
+
+  // Prompting the user to enter a name.
+  // Capturing the name input.
+  userInput = prompt(msgPrompt);
+
+  // Using that input to declare a variable.
+};
